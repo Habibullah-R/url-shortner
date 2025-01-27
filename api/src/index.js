2bcd04a87205c2093be07e23c13e2dbc9fa62da3
Habibullah-R <habibullahrashid150@gmail.com>
2025-01-27 10:44:34 +0500

    deploy

--- api/src/index.js
+++ api/src/index.js
@@ -5,10 +5,15 @@ import cors from "cors"
 import db from "./db/db.js"
 import urlRoutes from "./routes/url.routes.js"
 import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express()
 
 db();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.use(cors())
 app.use(express.json({limit:"16kb"}));
